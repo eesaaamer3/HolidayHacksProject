18226067d91e7c24902f950b000a0ead112e840e
Eesa Aamer <43073112+eesaaamer3@users.noreply.github.com>
2020-12-26 21:57:48 -0500

    Add files via upload

---
+++ Insight-Notifcation/Insight/InsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsightApp.swift
+//  Insight
+//
+//  Created by Eesa Aamer on 2020-12-26.
+//
+
+import SwiftUI
+
+@main
+struct InsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
